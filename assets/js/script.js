c048f6b4dc43b53cc94e5d0e0f135303f44130be
Mitch Henderson <shiftymitch@gmail.com>
2021-06-21 19:46:28 -0600

    style updates

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,1 @@
-let mainColor = getComputedStyle(document.documentElement).getPropertyValue("--main");
-let svgBrandColor = document.getElementById("brand-color").attributes[1].value;
 
-svgBrandColor = mainColor;
