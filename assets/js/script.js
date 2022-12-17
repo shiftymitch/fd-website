9491981a395cfe1661b07c4a28f6c000c7839f50
Mitch Henderson <shiftymitch@gmail.com>
2022-12-17 13:01:08 -0700

    added shopify checkout

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,13 @@
+window.onload = function() {
+
+    setTimeout(() => {
+        let iframes = document.getElementsByClassName("shopify-buy-frame");
+        let theframe = iframes[3].querySelector('iframe');
+        let body = theframe.contentWindow.document.querySelector('body');
+        let divwithBG = body.firstChild;
+
+        divwithBG.style.backgroundColor = "#ff362f";
+      }, "500")
+
+}
 
